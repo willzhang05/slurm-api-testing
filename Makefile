3378d92d378351d69f233072c5eae46d6aedcc19
William Zhang <willzhang05@gmail.com>
2019-08-02 21:18:22 +0000

    working resume

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS = -Wall -Wextra -ggdb3 --std=c99
 LIB =
 INC =
-main: main.c
-	gcc $(CFLAGS) $(LIB) $(INC) -o $@.o $^ -lslurm
+slurmutil: main.c
+	gcc $(CFLAGS) $(LIB) $(INC) -o $@ $^ -lslurm
 clean:
-	rm -f *.o *.out
+	rm -f slurmutil *.o *.out
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <slurm/slurm_errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 
 int suspend_all_jobs(job_info_msg_t* job_buffer_ptr) {
@@ -20,16 +21,52 @@ int suspend_all_jobs(job_info_msg_t* job_buffer_ptr) {
         }
         // slurm_print_job_info(stdout, job_ptr, 1);
     }
+    if (jobs_suspended == 0) {
+        printf("No jobs suspended.\n");
+    }
     return jobs_suspended;
 }
 
-int resume_all_jobs(job_info_msg_t* job_buffer_ptr) { return job_buffer_ptr->record_count; }
+int resume_all_jobs(job_info_msg_t* job_buffer_ptr) {
+    int jobs_resumed = 0;
+    job_info_t* job_ptr;
+    for (size_t i = 0; i < job_buffer_ptr->record_count; i++) {
+        job_ptr = &job_buffer_ptr->job_array[i];
+        if (job_ptr->job_state == JOB_SUSPENDED) {
+            int job_resume = slurm_resume(job_ptr->job_id);
+            if (job_resume == 0) {
+                printf("Job %d resumed.\n", job_ptr->job_id);
+                jobs_resumed++;
+            } else {
+                printf("Job %d failed to resume. Error code %d.\n", job_ptr->job_id, job_resume);
+            }
+        }
+    }
+    if (jobs_resumed == 0) {
+        printf("No jobs to resumed.\n");
+    }
+    return jobs_resumed;
+}
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Invalid arguments.\n\tUsage: ./slurmutil <option>\n");
+        return 1;
+    }
+
     job_info_msg_t* job_buffer_ptr = NULL;
 
     if (slurm_load_jobs((time_t)NULL, &job_buffer_ptr, SHOW_ALL)) {
         slurm_perror("slurm_load_jobs error");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "suspend") == 0) {
+        suspend_all_jobs(job_buffer_ptr);
+    } else if (strcmp(argv[1], "resume") == 0) {
+        resume_all_jobs(job_buffer_ptr);
+    } else {
+        fprintf(stderr, "Invalid arguments.\n\tUsage: ./slurmutil <option>\n");
         return 1;
     }
 
@@ -47,7 +84,6 @@ int main() {
     */
     // slurm_free_node_info_msg(node_buffer_ptr);
 
-    suspend_all_jobs(job_buffer_ptr);
     slurm_free_job_info_msg(job_buffer_ptr);
     return 0;
 }
